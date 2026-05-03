4b4d5ee6151716a58f72feb2f10a7e932752e612
Gabriel Calid Brito de Almeida <gabrielcalid@gmail.com>
2026-05-03 11:19:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -915,7 +915,7 @@ export default function App() {
                       )}
                     </div>
                   </div>
-                </div>
+                 </div>
               )}
 
               {activeSection === 'presentes' && (
